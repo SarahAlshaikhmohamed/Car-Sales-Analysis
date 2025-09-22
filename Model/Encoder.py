da996a755193a93d9ad5fc93482a8417cb373e72
SarahAlshaikhmohammed <sarah.alshaikh2@outlook.com>
2025-09-22 19:14:40 +0300

    encoder - decoder

---
+++ Model/Encoder.py
@@ -0,0 +1,13 @@
+from sklearn.preprocessing import OneHotEncoder
+
+encoder = OneHotEncoder(drop="first", sparse=False)
+
+# encoding function
+def encode(data):
+    encoded = encoder.fit_transform(data[["Manufacturer", "Model", "Fuel type"]])
+    return encoded
+
+# decoding function
+def decode(data):
+    decoded = encoder.inverse_transform(data)
+    return decoded
--- Model/Interface.py
+++ Model/Interface.py
@@ -7,7 +7,7 @@ import numpy as np
 import statsmodels.api as sm
 import gdown
 import os
-
+from Encoder import encode, decode
 
 # Load trained model using googledive
 MODEL_DIR = "Models"
@@ -42,15 +42,6 @@ dl_model   = keras.models.load_model(dl_model_path)
 
 app = FastAPI()
 
-FEATURES = [
-    "Engine size", "Year of manufacture", "Mileage",
-    "Manufacturer_Ford", "Manufacturer_Porsche", "Manufacturer_Toyota", "Manufacturer_VW",
-    "Model_911", "Model_Cayenne", "Model_Fiesta", "Model_Focus", "Model_Golf",
-    "Model_M5", "Model_Mondeo", "Model_Passat", "Model_Polo", "Model_Prius",
-    "Model_RAV4", "Model_X3", "Model_Yaris", "Model_Z4",
-    "Fuel type_Hybrid", "Fuel type_Petrol"
-]
-
 # Define request body schema
 class PredictionInput(BaseModel):
     engine_size: float
@@ -73,28 +64,7 @@ def predict(input_data: PredictionInput):
         from tensorflow import keras
         dl_model = keras.models.load_model(dl_model_path)
         
-    def encode_input(user: PredictionInput) -> pd.DataFrame:
-        row = {col: 0 for col in FEATURES}
-        row["Engine size"] = user.engine_size
-        row["Year of manufacture"] = user.year
-        row["Mileage"] = user.mileage
-
-        manuf_col = f"Manufacturer_{user.manufacturer}"
-        if manuf_col in row:
-            row[manuf_col] = 1
-
-        model_col = f"Model_{user.model}"
-        if model_col in row:
-            row[model_col] = 1
-
-        fuel_col = f"Fuel type_{user.fuel_type}"
-        if fuel_col in row:
-            row[fuel_col] = 1
-
-        return pd.DataFrame([row], columns=FEATURES)
-
-    # Encode input
-    data = encode_input(input_data)
+    data = encode(input_data)
 
     # --- Statsmodels ---
     data_sm = sm.add_constant(data, has_constant="add")
