12b25bfed7f85286fe79ed7283ac794e75ebba75
SarahAlshaikhmohammed <sarah.alshaikh2@outlook.com>
2025-10-05 21:16:39 +0300

    encoder decoder

--- Model/Encoder.py
+++ Model/Encoder.py
@@ -1,13 +1,16 @@
 from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+import numpy as np
 
 encoder = OneHotEncoder(drop="first")
 
-# encoding function
-def encode(data):
-    encoded = encoder.fit_transform(data[["Manufacturer", "Model", "Fuel type"]])
-    return encoded
+def encode(data: dict):
+    df = pd.DataFrame([data]) 
+    encoded = encoder.fit_transform(df[["manufacturer", "model", "fuel_type"]])
+    num_features = df[["engine_size", "year", "mileage"]].values
+    X = np.hstack([num_features, encoded.toarray()])
+    return X
 
-# decoding function
 def decode(data):
     decoded = encoder.inverse_transform(data)
     return decoded
